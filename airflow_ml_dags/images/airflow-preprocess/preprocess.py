f56762c63cab2f0c8c799becbbd577127df4f7b9
rchermanteev <rchermanteev@bk.ru>
2021-06-09 17:25:41 +0300

    add solution hw3

---
+++ airflow_ml_dags/images/airflow-preprocess/preprocess.py
@@ -0,0 +1,20 @@
+import os
+
+import click
+import pandas as pd
+
+
+@click.command()
+@click.option("--input-dir")
+@click.option("--output-dir")
+def preprocess(input_dir: str, output_dir: str):
+    data = pd.read_csv(os.path.join(input_dir, "data.csv"))
+    target = pd.read_csv(os.path.join(input_dir, "target.csv"))
+    # here we can doing some prepare data
+    os.makedirs(output_dir, exist_ok=True)
+    data.to_csv(os.path.join(output_dir, "data.csv"), index=False)
+    target.to_csv(os.path.join(output_dir, "target.csv"), index=False)
+
+
+if __name__ == "__main__":
+    preprocess()
